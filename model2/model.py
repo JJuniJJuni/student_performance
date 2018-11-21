39ac4c450c8a5a2c39718ea8edda40398b67b454
JJuniJJuni <gud305@gmail.com>
2018-11-21 17:29:32 +0900

    add test traing data

--- model2/model.py
+++ model2/model.py
@@ -10,27 +10,28 @@ from preprocess import split_data
 class NeuralNet(nn.Module):
     def __init__(self, input_size, output_size):
         super(NeuralNet, self).__init__()
-        self.input_layer = nn.Linear(input_size, 28)
-        self.L1 = nn.Linear(28, 24)
-        self.L2 = nn.Linear(24, 20)
-        self.L3 = nn.Linear(20, 16)
-        self.output_layer = nn.Linear(16, output_size)
+        self.input_layer = nn.Linear(input_size, 22)
+        self.L1 = nn.Linear(22, 16)
+        self.L2 = nn.Linear(16, 10)
+        # self.L3 = nn.Linear(20, 16)
+        self.output_layer = nn.Linear(10, output_size)
 
     def forward(self, x):
-        x = F.relu(self.input_layer(x))
+        x = torch.sigmoid(self.input_layer(x))
         x = F.relu(self.L1(x))
         x = F.relu(self.L2(x))
-        x = F.relu(self.L3(x))
+        # x = F.relu(self.L3(x))
         x = self.output_layer(x)
         return x
 
 
 input_matrix, target_matrix = preprocess('./data/student-por.csv')
+epoch, device = 12000, torch.device('cpu')
 x_train, x_test, y_train, y_test = split_data(input_matrix, target_matrix)
-x_train = torch.tensor(x_train, device=torch.device('cuda:0'), dtype=torch.float, requires_grad=False)
-x_test = torch.tensor(x_test, device=torch.device('cuda:0'), dtype=torch.float, requires_grad=False)
-y_train = torch.tensor(y_train, device=torch.device('cuda:0'), dtype=torch.float, requires_grad=False)
-y_test = torch.tensor(y_test, device=torch.device('cuda:0'), dtype=torch.float, requires_grad=False)
+x_train = torch.tensor(x_train, device=device, dtype=torch.float, requires_grad=False)
+x_test = torch.tensor(x_test, device=device, dtype=torch.float, requires_grad=False)
+y_train = torch.tensor(y_train, device=device, dtype=torch.float, requires_grad=False)
+y_test = torch.tensor(y_test, device=device, dtype=torch.float, requires_grad=False)
 model = NeuralNet(32, 1)
 print('[Model Structure]')
 print(model)
@@ -38,7 +39,7 @@ print(model)
 criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-for _ in range(20000):
+for idx in range(epoch):
     # x_train.to(device), y_train.to(device)
     output_data = model(x_train.to(device=torch.device('cpu')))
     loss = criterion(torch.squeeze(output_data), y_train.to(device=torch.device('cpu')))
@@ -46,20 +47,27 @@ for _ in range(20000):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+    if idx % 50 == 0:
+        print('Train Epoch: {}/{}\tLoss: {:.6f}'.format(
+            idx, epoch, loss.data[0]))
+train_correct = 0
 
 
-model.eval()
-test_loss = 0
-correct = 0
-
-print('[Test]')
-for data, target in zip(x_test, y_test):
-    output_data = int(model(data.to(device=torch.device('cpu'))))
-    # print(output_data)
-    # value = int(torch.argmax(output_data))
+for data, target in zip(x_train, y_train):
+    output_data = float(model(data.to(device=torch.device('cpu'))))
     print(output_data, target)
     if round(output_data) == target:
-        correct += 1
-print(100*(correct/len(y_test)))
-print('[Prediction]')
+        train_correct += 1
+print('Train Percentage:', 100*(train_correct/len(y_train)))
+
+# model.eval()
+test_correct = 0
+
+# print('[Test]')
+for data, target in zip(x_test, y_test):
+    output_data = float(model(data.to(device=torch.device('cpu'))))
+    # print(output_data, target, bool(output_data == target))
+    if round(output_data) == target:
+        test_correct += 1
+print('Test Percentage:', 100*(test_correct/len(y_test)))
 
