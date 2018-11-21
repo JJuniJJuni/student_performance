4e96ce55908b41fa4194075bf676395a9c145185
JJuniJJuni <gud305@gmail.com>
2018-11-21 23:33:32 +0900

    add init function xavior

--- model2/model.py
+++ model2/model.py
@@ -7,12 +7,11 @@ import numpy as np
 
 
 from preprocess import preprocess
-from preprocess import split_data
 from preprocess import cross_validation
 
 
 start_time = time.time()
-epoch, device = 5000, torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+epoch, device = 100000, torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 input_mat, target_mat, features_counts = preprocess('./data/student-por.csv')
 input_por, target_por, _ = preprocess('./data/student-mat.csv')
 input_matrix = torch.tensor(np.concatenate((input_mat, input_por), 0), device=device,
@@ -24,17 +23,17 @@ target_matrix = torch.tensor(np.concatenate((target_mat, target_por), 0), device
 class NeuralNet(nn.Module):
     def __init__(self, input_size, output_size):
         super(NeuralNet, self).__init__()
-        self.input_layer = nn.Linear(input_size, 22)
-        self.L1 = nn.Linear(22, 16)
-        self.L2 = nn.Linear(16, 10)
-        # self.L3 = nn.Linear(20, 16)
-        self.output_layer = nn.Linear(10, output_size)
+        self.input_layer = nn.Linear(input_size, 16)
+        self.L1 = nn.Linear(16, 12)
+        self.L2 = nn.Linear(12, 8)
+        self.L3 = nn.Linear(8, 4)
+        self.output_layer = nn.Linear(4, output_size)
 
     def forward(self, x):
-        x = torch.sigmoid(self.input_layer(x))
-        x = F.relu(self.L1(x))
-        x = F.relu(self.L2(x))
-        # x = F.relu(self.L3(x))
+        x = F.relu(self.input_layer(x))
+        x = torch.sigmoid(self.L1(x))
+        x = torch.sigmoid(self.L2(x))
+        x = torch.sigmoid(self.L3(x))
         x = self.output_layer(x)
         return x
 
@@ -45,7 +44,6 @@ criterion = nn.MSELoss()
 
 def train(epoch, input_model, input_training, input_target):
     for idx in range(epoch):
-        # x_train.to(device), y_train.to(device)
         output_data = input_model(input_training.to(device=torch.device('cpu')))
         loss = criterion(torch.squeeze(output_data), input_target.to(device=torch.device('cpu')))
         model.zero_grad()
@@ -54,33 +52,49 @@ def train(epoch, input_model, input_training, input_target):
         optimizer.step()
         if idx % 50 == 0:
             print('Train Epoch: {}/{}\tLoss: {:.6f}'.format(idx, epoch, loss.data[0]))
+    return loss.data[0]
 
 
 def test(text, input_model, input_train, input_target):
     correct = 0
     for data, target in zip(input_train, input_target):
         output_data = float(input_model(data.to(device=torch.device('cpu'))))
-        # print(output_data, target)
         if round(output_data) == target:
             correct += 1
-    print('{} Percentage:'.format(text), 100 * (correct / len(input_target)))
+    percentage = round(100 * (correct / len(input_target)))
+    print('{} Percentage:{}%({}/{})'.format(text, percentage, correct, len(input_target)))
+    return percentage
 
 
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        size = m.weight.size()
+        fan_out = size[0]  # number of rows
+        fan_in = size[1]  # number of columns
+        variance = np.sqrt(2.0 / (fan_in + fan_out))
+        m.weight.data.normal_(0.0, variance)
+
+
+train_value, test_value = [], []
 for idx, (x_test, y_test) in enumerate(zip(input_arrays, target_arrays)):
-    print('R{} Model Structure'.format(idx+1))
+    # print('R{} Model Structure'.format(idx+1))
     model = NeuralNet(features_counts, 1)
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    model.apply(init_weights)
+    optimizer = optim.SGD(model.parameters(), lr=0.05)
     x_train = torch.tensor([], device=device, dtype=torch.float, requires_grad=False)
-    # print(x_train)
     y_train = torch.tensor([], device=device, dtype=torch.float, requires_grad=False)
     for train_idx, (x, y) in enumerate(zip(input_arrays, target_arrays)):
         if train_idx != idx:
             x_train = torch.cat((x_train, x), 0)
             y_train = torch.cat((y_train, y), 0)
-    train(epoch, model, x_train, y_train)
-    test('R{} Train'.format(idx+1), model, x_train, y_train)
-    test('R{} Test'.format(idx+1), model, x_test, y_test)
+    loss = train(epoch, model, x_train, y_train)
+    print('R{} Train Loss: {}'.format(idx+1, loss))
+    train_value.append(test('R{} Train'.format(idx+1), model, x_train, y_train))
+    test_value.append(test('R{} Test'.format(idx+1), model, x_test, y_test))
+    print()
 print("time: {}".format(time.time()-start_time))
-# model.eval()
-# print('[Test]')
+overall_train = round(sum(train_value) / 10)
+overall_test = round(sum(test_value) / 10)
+print("overall train's percentage: {}%, test's percentage: {}%".format(overall_train, overall_test))
+
 
